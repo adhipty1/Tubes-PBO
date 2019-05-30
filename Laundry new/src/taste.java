ac4c45344026a12a69abe1073ae46bf97251eaa4
adhipty1 <adhigana@webmail.umm.ac.id>
2019-05-30 12:41:57 +0700

    Abstract class

---
+++ Laundry new/src/taste.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Acer
+ */
+public abstract class taste {
+     public abstract String getNama();
+     public abstract int getterima();
+     public abstract int getambil();
+     public abstract int getkg();
+     public abstract int getpaket();
+     public abstract int getharga();
+   }
